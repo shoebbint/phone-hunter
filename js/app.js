1b2588d8f713066b0c15dd288fa4123e63220b24
USER <shoebbint@gmail.com>
2022-03-02 13:47:45 +0600

    style-added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // search-button-function
 const phoneSearch = () => {
     document.getElementById('phone-details').innerHTML = "";
+
     let searchText = document.getElementById("search-field").value;
     //search api 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
@@ -9,11 +10,13 @@ const phoneSearch = () => {
         .then(res => res.json())
         .then(data => showPhones(data.status, data.data))
     console.log(data)
-    document.getElementById("search-field").value = "";
+
 }
-const showPhones = (apiData, phones) => {
+const showPhones = (apiData, phoneResults) => {
+    document.getElementById("search-field").value = "";
     const searchResult = document.getElementById('search-result');
     const noResultDiv = document.getElementById('no-result');
+    const phones = phoneResults.slice(0, 20);
     if (apiData == true) {
 
         noResultDiv.style.display = "none";
@@ -25,13 +28,13 @@ const showPhones = (apiData, phones) => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-        <div class="card " style="width: 18rem;">
-            <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card border-0" style="width: 18rem;">
+            <img src="${phone.image}" class="card-img-top " alt="...">
             <div class="card-body">
             <h5 class="card-title text-center">${phone.phone_name}</h5>
             <p class="card-text text-center">Brand:${phone.brand}</p>
             <div class="text-center">
-            <a href="#" onclick="seeDetails('${phone.slug}')" id="details-btn" class="btn btn-primary mx-auto">See Details</a>
+            <a href="#" onclick="seeDetails('${phone.slug}')" id="details-btn" class="btn btn-outline-warning mx-auto">See Details</a>
             </div>
             </div>
         </div>`
@@ -55,54 +58,35 @@ const seeDetails = (id) => {
     const showDetails = (details) => {
         const phoneDetails = document.getElementById('phone-details');
         phoneDetails.innerHTML = "";
-        console.log(details)
+        console.log(details.others);
 
-
-        if (details.releaseDate == '') {
-            console.log('bal')
-            const div2 = document.createElement('div');
-            div2.innerHTML = `
+        const div = document.createElement('div');
+        div.innerHTML = `
             <div class="card h-100 w-50 mx-auto">
                 <img class="mx-auto" src="${details.image}" class="card-img-top w-50" alt="...">
                 <div class="card-body">
                 <h5 class="card-title text-center">${details.name}</h5>
-                <p class="card-text text-center">Release Date:Not Found</p>
+                <p class="card-text text-center">Release Date: ${details.releaseDate ? details.releaseDate : "Not Found"}</p>
                 </div>
                 <div>
                 <ul class="list-group list-group-flush">
                 <li class="list-group-item text-center">Storage: ${details.mainFeatures.storage}</li>
                 <li class="list-group-item text-center">Display: ${details.mainFeatures.displaySize}</li>
                 <li class="list-group-item text-center">Chip: ${details.mainFeatures.chipSet}</li>
+                <li class="list-group-item text-center">Sensors: ${details.mainFeatures.sensors}</li>
+                <li class="list-group-item text-center"><b>Others info:</b> <br> 
+                <b>Bluetooth:</b>${details.others ? details.others.Bluetooth : "Not Found"} <br>
+                <b>GPS :</b> ${details.others ? details.others.GPS : "Not Found"} <br>
+                <b>NFC :</b> ${details.others ? details.others.NFC : "Not Found"} <br>
+                <b>Radio :</b>  ${details.others ? details.others.Radio : "Not Found"} <br>
+                <b>USB :</b> ${details.others ? details.others.USB : "Not Found"} <br>
+                <b>WLAN :</b> ${details.others ? details.others.WLAN : "Not Found"} <br>
+                   
+                    </li>
                 </ul>
                 </div>
-                <div class="card-footer">
-                <small class="text-muted">${details.releaseDate}</small>
-                </div>
             </div>`
-            phoneDetails.appendChild(div2);
-        }
-        else {
-            const div = document.createElement('div');
-            div.innerHTML = `
-            <div class="card h-100 w-50 mx-auto">
-                <img class="mx-auto" src="${details.image}" class="card-img-top w-50" alt="...">
-                <div class="card-body">
-                <h5 class="card-title text-center">${details.name}</h5>
-                <p class="card-text text-center">${details.releaseDate}</p>
-                </div>
-                <div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item text-center">Storage: ${details.mainFeatures.storage}</li>
-                <li class="list-group-item text-center">Display: ${details.mainFeatures.displaySize}</li>
-                <li class="list-group-item text-center">Chip: ${details.mainFeatures.chipSet}</li>
-                </ul>
-                </div>
-                <div class="card-footer">
-                <small class="text-muted">${details.releaseDate}</small>
-                </div>
-            </div>`
-            phoneDetails.appendChild(div);
-        }
+        phoneDetails.appendChild(div);
 
     }
 }
